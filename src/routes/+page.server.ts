ea4be8e6c2a6fa0929aff88a2cb217e033cc4f01
Dominik Lenz <dominik@notabot.cc>
2023-07-25 00:32:10 +0200

    hacks some layout fixes

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,11 @@ import {loadPresentInfo, loadPrices, loadSpecialOffer} from '$lib/server/cms';
 
 export const prerender = true;
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async (): Promise<{
+    present: { location: string, date: Date }[],
+    prices: { name: string, price: number }[],
+    specialOffer: { name: string, image: string, description: string, available: boolean }[]
+}> => {
     const presentInfo = await loadPresentInfo();
     const prices = await loadPrices();
     const specialOffer = await loadSpecialOffer();
@@ -12,7 +16,18 @@ export const load: PageServerLoad = async () => {
     console.log("Loaded special offer:", specialOffer);
     return {
         present: presentInfo,
-        prices: prices,
+        prices: prices /*[
+            {name: "Kaffee", price: 1.5},
+            {name: "Cappuccino", price: 2.5},
+            {name: "Latte Macchiato", price: 3.5},
+            {name: "Espresso", price: 1.5},
+            {name: "Espresso Macchiato", price: 2.5},
+            {name: "Milchkaffee", price: 2.5},
+            {name: "Kakao", price: 2.5},
+            {name: "Tee", price: 1.5},
+            {name: "Heiße Schokolade", price: 2.5},
+
+        ]*/,
         specialOffer: specialOffer
     };
 }
